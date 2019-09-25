e9c463678e535910a0ab4383fce983d936d7f793
Wendy Harris <wendyv510@hotmail.com>
2019-09-25 23:45:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,3 +9,12 @@ songs = [
   "Harry Chapin - Cats in the Cradle", 
   "Amos Lee- Keep it Loose, Keep it Tight"
   ]
+  
+  def say_hello(name)
+    "Hi #{name}!"
+  end 
+  
+  puts "Enter your name:"
+  users_name = gets.strip
+  
+  puts say_hello(users_name) 
