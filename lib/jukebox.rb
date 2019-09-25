65f42de03040e84ed213dccf992befe0cda917b7
Wendy Harris <wendyv510@hotmail.com>
2019-09-25 23:44:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,11 @@
-# Add your code here
+songs = [
+  "Pheonix -1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too much",
+  "The Naked and The Famous- Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Pheonix -Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle", 
+  "Amos Lee- Keep it Loose, Keep it Tight"
+  ]
